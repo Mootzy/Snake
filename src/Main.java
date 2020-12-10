6186ff2cbba1d3c9c4f6d4a21a28f7cc8d8d0ae7
Mootzy <tyler77156@gmail.com>
2020-12-10 13:08:50 -0700

    added score tracker, gameover red text to main frame. fixed bug where game couldn't properly restart. I'd still like to add a pause function as well as a ability to live feed score throughout the game but i feel it serves it's purpose for the time being. now onto nevermore.

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,34 @@ public class Main {
 
     public static void main(String[] args) {
         new MainMenuFrame();
-       //new Frame();
-
-
 
     }
 }
+
+
+
+/*
+
+- Tyler Wallace December 9th final project -
+
+Where to begin... I experienced a multitude of issues/problems with this program. most noteably-
+1- collision detection
+2- creating snake
+3- detecting how many game units were available on the JPanel
+4- compensation for strange movement patterns i.e turning 180/360 degrees.
+5- score keeping
+6- gathering input from users keyboard
+7- random generation of 'apple' on screen
+
+Overall as my git commit's will reveal i'd still like to add a pause function when the player hit's space,
+as well as the ability to live track the players score in game.
+
+since the presentation i've added a "reset" and "quit" button when the player loses
+as well as a bright red "game over" with the players score. I've also left the alert dialog as i feel
+placing the buttons on the jPanel its self may provide a more aesthetically pleasing look it will ultimately
+take more time.
+
+Thanks for the class.
+
+
+ */
